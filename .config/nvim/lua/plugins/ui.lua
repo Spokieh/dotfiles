7fc31d15036c3c31eca818df4171d4dfeeff6dbc
Spokieh <dohanyos96@gmail.com>
2024-05-29 11:25:45 +0200

    nvim config saved for lazyvim

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+    end,
+  },
+
+  -- logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+███████╗██████╗  ██████╗ ██╗  ██╗██╗███████╗██╗  ██╗
+██╔════╝██╔══██╗██╔═══██╗██║ ██╔╝██║██╔════╝██║  ██║
+███████╗██████╔╝██║   ██║█████╔╝ ██║█████╗  ███████║
+╚════██║██╔═══╝ ██║   ██║██╔═██╗ ██║██╔══╝  ██╔══██║
+███████║██║     ╚██████╔╝██║  ██╗██║███████╗██║  ██║
+╚══════╝╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝
+    ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
